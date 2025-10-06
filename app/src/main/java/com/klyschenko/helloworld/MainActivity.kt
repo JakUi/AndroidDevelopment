1b260fa2b64aba27e19ff526e56c24caadce1095
Konstantin <klyschenko@ourgapps.com>
2025-10-06 14:07:25 +0400

    Scaffold and Custom components

--- app/src/main/java/com/klyschenko/helloworld/MainActivity.kt
+++ app/src/main/java/com/klyschenko/helloworld/MainActivity.kt
@@ -11,7 +11,9 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -19,75 +21,76 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val characters = "Hello world!".filter {
-            it.isLetter()
-        }.map {
-            it.uppercase()
-        }
-        val firstRow = characters.take(5)
-        val secondRow = characters.takeLast(5)
-        setContent {
-            val green = Color(0xFF79A86A)
-            val white = Color(0xFFFFFFFF)
-            val grey = Color(0xFF797D80)
-            val yellow = Color(0xFFC6B567)
 
-            Column(
-                modifier = Modifier
-                    .padding(top = 8.dp)
-                    .fillMaxWidth()
-                    .background(white)
-                    .padding(8.dp),
-                verticalArrangement = Arrangement.spacedBy(8.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
+        setContent {
+            Scaffold { paddingValues ->
+                Greeting(
+                    modifier = Modifier.padding(paddingValues),
+                    text = "Hello World! Hello World!"
+                )
+            }
+
+        }
+    }
+
+    @Composable
+    private fun Greeting(
+        modifier: Modifier = Modifier,
+        text: String
+    ) {
+
+        val words = text.split(" ").map { word ->
+            word.filter { it.isLetter() }.uppercase()
+        }
+
+        val green = Color(0xFF79A86A)
+        val white = Color(0xFFFFFFFF)
+        val grey = Color(0xFF797D80)
+        val yellow = Color(0xFFC6B567)
+
+        Column(
+            modifier = modifier
+                .fillMaxWidth()
+                .background(white)
+                .padding(8.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            words.forEachIndexed { rawIndex, word ->
                 Row(
                     modifier = Modifier
                         .fillMaxWidth(),
                     horizontalArrangement = Arrangement.spacedBy(8.dp),
                 ) {
-                    for ((index, char) in firstRow.withIndex()) {
-                        Box(
-                            modifier = Modifier
-                                .background(color = if (index == 2 || index == 4) yellow else grey)
-                                .fillMaxWidth()
-                                .weight(1f)
-                                .aspectRatio(1f),
-                            contentAlignment = Alignment.Center
-                        ) {
-                            Text(
-                                text = char,
-                                fontSize = 32.sp,
-                                fontWeight = FontWeight.ExtraBold,
-                                color = white,
-                                textAlign = TextAlign.Center
-                            )
+                    for (char in word) {
+                        val color = if (rawIndex % 2 == 0) {
+                            val isYellow = Random.nextBoolean()
+                            if (isYellow) {
+                                yellow
+                            } else {
+                                grey
+                            }
+                        } else {
+                            green
                         }
-                    }
-                }
-                Row(
-                    modifier = Modifier
-                        .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.spacedBy(8.dp),
-                ) {
-                    for (char in secondRow) {
                         Box(
                             modifier = Modifier
-                                .background(color = green)
+                                .background(color = color)
                                 .fillMaxWidth()
                                 .weight(1f)
                                 .aspectRatio(1f),
                             contentAlignment = Alignment.Center
                         ) {
                             Text(
-                                text = char,
+                                text = char.toString(),
+                                fontSize = 32.sp,
                                 fontWeight = FontWeight.ExtraBold,
                                 color = white,
-                                fontSize = 32.sp,
                                 textAlign = TextAlign.Center
                             )
                         }
@@ -96,4 +99,5 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+
 }
